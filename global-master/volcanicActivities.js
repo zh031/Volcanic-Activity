25164d436878a4dcaee037a655b1c84acb97546e
zh031 <zuhan0317@gmail.com>
2024-12-20 20:09:04 -0600

    volcano container id

--- global-master/volcanicActivities.js
+++ global-master/volcanicActivities.js
@@ -3,7 +3,21 @@ function fetchVolcanicData(){
     fetch('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-01-01&endtime=2022-12-31&minmagnitude=2&maxlatitude=15&minlatitude=13&maxlongitude=-88&minlongitude=-92')
         .then(response => response.json())
         // fetch multiple types of data 
-        .then(data => {
+        .then(data => { 
+            const volcanoContainer= document.getElementById('volcanoContainer');
+
+            //Clear existing data inside volcanicContainer
+            volcanoContainer.innerHTML = '';
+
+            //Check if data(from line 6) exists 
+            if (data.features.length === 0) {
+                volcanoContainer.innerHTML = '<p>No significant volcanic activity recorded</p>';
+                return;
+            }
+
+            //Only runs when data exists ( is not equal to 0)
+             
+
             data.features.forEach(feature => {
                 console.log('volcanic activity in Guatemala');
                 console.log('location: ' + feature.properties.place);
@@ -15,7 +29,6 @@ function fetchVolcanicData(){
             console.error('error fetching volcanic data',error);
         });
 
-    const volcanoContainer= document.getElementById('volcanoContainer');
     const volcanoData= document.getElementById('volcanoData');
 }
 fetchVolcanicData();
