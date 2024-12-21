5f0675b19be2a06bf6ee6f03c188bb666fc13c9c
zh031 <zuhan0317@gmail.com>
2024-12-20 21:12:15 -0600

    axios and data fetch complete

--- global-master/volcanicActivities.js
+++ global-master/volcanicActivities.js
@@ -1,9 +1,16 @@
 // This is a function for fetching volcanic data from USGS
 function fetchVolcanicData(){
     fetch('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-01-01&endtime=2022-12-31&minmagnitude=2&maxlatitude=15&minlatitude=13&maxlongitude=-88&minlongitude=-92')
-        .then(response => response.json())
+        .then(response => {
+            console.log('http response', response.status);
+            if (!response.ok){
+                //error not for console
+                throw new Error(`http error ${response.status}`);
+            } return response.json();
+        })
         // fetch multiple types of data 
         .then(data => { 
+            console.log('data: ', data)
             const volcanoContainer= document.getElementById('volcanoContainer');
 
             //Clear existing data inside volcanicContainer
@@ -19,16 +26,33 @@ function fetchVolcanicData(){
              
 
             data.features.forEach(feature => {
-                console.log('volcanic activity in Guatemala');
-                console.log('location: ' + feature.properties.place);
-                console.log('magnitude: ' + feature.properties.mag);
-                console.log('time: ' + new Date(features.properties.time).toLocaleString());
+            
+                console.log('place information', feature.properties.place || 'Unknown location');
+                console.log('magnitude information', feature.properties.mag || 'Not applicable');
+
+                const place = feature.properties.place || 'Unknown location';
+                const magnitude = feature.properties.mag || 'Not applicable';
+                const time = feature.properties.time ? new Date(features.properties.time).toLocaleString() : 'Unknwon time';
+
+                const activityElement = document.createElement('div');
+                activityElement.classList.add('volcano-acitivity');
+
+                // displaying in HTML
+                activityElement.innerHTML = `
+                    <p><strong>Earthquake Activity Site</strong> ${place}</p>
+                    <p><strong>Earthquake Magnitude</strong> ${magnitude}</p>
+                    <p><strong>Earthquake Activity Duration</strong> ${time}</p>
+                `;
+                volcanoContainer.appendChild(activityElement);
             });
         })
         .catch(error => {
-            console.error('error fetching volcanic data',error);
+            console.error('error fetching volcanic data', error.message);
+            const volcanoContainer = document.getElementById('volcanoContainer');
+            volcanoContainer.innerHTML = '<p>Error in fetching volcano data</p>';
+
+
         });
 
-    const volcanoData= document.getElementById('volcanoData');
 }
 fetchVolcanicData();
